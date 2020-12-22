2bb896bb6123fd2390d3372db38280fe12c71629
Mátyás Budavári <33528704+budavariam@users.noreply.github.com>
2020-12-22 19:23:50 +0100

    Add functionality - Connect a buzzer to P0 in order to hear the melody

--- main.ts
+++ main.ts
@@ -1,1 +1,43 @@
-
+input.onButtonPressed(Button.A, function () {
+    lit = true
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.OnceInBackground)
+})
+input.onButtonPressed(Button.B, function () {
+    lit = !(lit)
+})
+input.onGesture(Gesture.Shake, function () {
+    lit = false
+})
+let lit = false
+basic.showIcon(IconNames.SmallHeart)
+basic.showIcon(IconNames.Heart)
+lit = false
+music.setVolume(255)
+basic.forever(function () {
+    if (lit) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.pause(randint(0, 300))
+        basic.showLeds(`
+            . . . # .
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.pause(randint(100, 500))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
+})
